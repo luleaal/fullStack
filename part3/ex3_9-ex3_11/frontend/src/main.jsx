cc3f63fcf40debe05a8593fe645636d6e87fbba9
lulemar16 <100451897@alumnos.uc3m.es>
2023-10-05 19:42:57 +0300

    m

--- part3/ex3_9-ex3_11/frontend/src/main.jsx
+++ part3/ex3_9-ex3_11/frontend/src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
