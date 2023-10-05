a31c12709b66eb90879424d4a6a2afc831e9fced
lulemar16 <100451897@alumnos.uc3m.es>
2023-10-05 19:38:16 +0300

    m

--- part3/ex3_9-ex3_11/frontend/vite.config.js
+++ part3/ex3_9-ex3_11/frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
+  server: {
+    host: '0.0.0.0', // This allows external access
+    port: 5173, // The port you want to use
+  }, 
   plugins: [react()],
-})
+};
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
