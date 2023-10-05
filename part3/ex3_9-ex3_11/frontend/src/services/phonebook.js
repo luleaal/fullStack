770614ed56d6e0b943c550ecc5cd5a0644392711
lulemar16 <100451897@alumnos.uc3m.es>
2023-10-05 19:51:16 +0300

    m

--- part3/ex3_9-ex3_11/frontend/src/services/phonebook.js
+++ part3/ex3_9-ex3_11/frontend/src/services/phonebook.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const baseUrl = '/api/persons' 
+const baseUrl = 'https://phonebook-render-front.onrender.com/' 
 
 const getAll = () => {
   return axios.get(baseUrl).then((response) => response.data);
