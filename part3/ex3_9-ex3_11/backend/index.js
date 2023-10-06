bff32ed728f4fe2375ac65a002dee0514139131f
lulemar16 <100451897@alumnos.uc3m.es>
2023-10-06 18:50:57 +0300

    index

--- part3/ex3_9-ex3_11/backend/index.js
+++ part3/ex3_9-ex3_11/backend/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 let persons = require('./db.json');
-// const cors = require('cors')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.static('dist'))
 
-// const morgan = require('morgan'); 
+const morgan = require('morgan'); 
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -20,14 +20,14 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-// app.use(cors())
+pp.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
-// morgan.token('postData', (req) => {
-//   return JSON.stringify(req.body);
-// });
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
+morgan.token('postData', (req) => {
+   return JSON.stringify(req.body);
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
 
 
 app.get('/api/persons', (request, response) => {
