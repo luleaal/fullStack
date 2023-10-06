f5ef7ba1dcbf5f10ecb1ca90ac65c06df1fc8b25
lulemar16 <100451897@alumnos.uc3m.es>
2023-10-06 10:43:45 +0300

    m

--- part3/ex3_9-ex3_11/backend/index.js
+++ part3/ex3_9-ex3_11/backend/index.js
@@ -23,7 +23,7 @@ morgan.token('postData', (req) => {
 });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
 app.use(requestLogger)
-app.use(express.static('build'))
+app.use(express.static('dist'))
 
 
 app.get('/persons', (request, response) => {
