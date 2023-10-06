84e74f4bcb83acc933b2405ebf6dead0765ca5bc
lulemar16 <100451897@alumnos.uc3m.es>
2023-10-06 16:02:15 +0300

    m

--- part3/ex3_9-ex3_11/backend/index.js
+++ part3/ex3_9-ex3_11/backend/index.js
@@ -66,7 +66,7 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/persons', (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
