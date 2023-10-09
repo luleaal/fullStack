61c7f4cf1f61cb9a2d112c4129c324fa1323c399
lulemar16 <100451897@alumnos.uc3m.es>
2023-10-09 18:18:09 +0300

    correction

--- part3/ex3_9-ex3_11/frontend/src/services/phonebook.js
+++ part3/ex3_9-ex3_11/frontend/src/services/phonebook.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const baseUrl = 'https://phonebook-render-c33s.onrender.com//api/persons'; 
+const baseUrl = '/api/persons'; 
 
 const getAll = () => {
   return axios.get(baseUrl).then((response) => response.data);
