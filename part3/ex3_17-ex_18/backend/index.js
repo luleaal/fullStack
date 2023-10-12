baa0ec37923312931fc4a62f392472c7c3de7ecb
lulemar16 <100451897@alumnos.uc3m.es>
2023-10-12 12:26:02 +0300

    exercises17_18

---
+++ part3/ex3_17-ex_18/backend/index.js
@@ -0,0 +1,124 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const Person = require('./models/person')
+
+const app = express()
+
+app.use(express.static('dist'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+});
+
+
+app.get('/persons/:id', (request, response) => {
+  Person.findById(request.params.id)
+      .then(person => {
+        if (person) {
+          response.json(person)
+        } else {
+          response.status(404).end()
+        }  
+      })
+      .catch(error => next(error))
+})
+// http://localhost:3001/api/persons/1
+
+app.get('/info', (request, response) => {
+    const num = persons.length
+    const now = new Date().toString();
+    const resp = "Phonebook has info for " + num + " people <br/> " + now
+
+    response.send(resp)
+  })
+// http://localhost:3001/info
+
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndDelete(request.params.id)
+      .then(person => {
+          response.status(204).end()
+      })
+      .catch(error => next(error))
+})
+
+
+// const generateId = () => {
+//   const maxId = persons.length > 0
+//     ? Math.max(...persons.map(n => n.id))
+//     : 0
+//   return maxId + 1
+// }
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+      return response.status(400).json({
+          error: 'Name and/or number missing'
+      })
+  }
+
+  const person = new Person({
+      name: body.name,
+      number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+      response.json(savedPerson)
+  })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ part3/ex3_17-ex_18/backend/mongo.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('Usage: node mongo.js <password> [<name> <number>]');
+  process.exit(1);
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://luciale:${password}@cluster0.xm9ig8c.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=AtlasApp`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length === 3) {
+  // If only password provided, list all persons
+  Person.find({}).then((persons) => {
+    console.log('phonebook:');
+    persons.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+  // If password, name, and number provided, add a new person
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const newPerson = new Person({
+    name,
+    number,
+  });
+
+  newPerson.save().then(() => {
+    console.log(`Added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  console.log('Invalid number of arguments.');
+  mongoose.connection.close();
+}
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
+
