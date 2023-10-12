df4b3a4377b82695b9f8ee949ab6ba6858e0a2a5
lulemar16 <100451897@alumnos.uc3m.es>
2023-10-12 12:39:48 +0300

    exercise3_19

---
+++ part3/ex3_19/frontend/src/services/phonebook.js
@@ -0,0 +1,23 @@
+// services/phonebook.js
+
+import axios from 'axios';
+
+const baseUrl = '/api/persons'; 
+
+const getAll = () => {
+  return axios.get(baseUrl).then((response) => response.data);
+};
+
+const create = (newPerson) => {
+  return axios.post(baseUrl, newPerson).then((response) => response.data);
+};
+
+const eliminate = (id) => {
+  return axios.delete(`${baseUrl}/${id}`).then((response) => response.data);
+};
+
+export default {
+  getAll,
+  create,
+  eliminate,
+};
