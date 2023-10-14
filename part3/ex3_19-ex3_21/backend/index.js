f945b6ff0464f11f849d5a741edb56c762ba9b8c
lulemar16 <100451897@alumnos.uc3m.es>
2023-10-14 19:50:55 +0300

    exercise20

--- part3/ex3_19-ex3_21/backend/index.js
+++ part3/ex3_19-ex3_21/backend/index.js
@@ -99,6 +99,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
       response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
