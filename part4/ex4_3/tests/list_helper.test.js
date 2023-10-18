8cf38bdf3442439216860a85c45580de8c33c557
lulemar16 <100451897@alumnos.uc3m.es>
2023-10-18 10:15:53 +0300

    exercise4

--- part4/ex4_3/tests/list_helper.test.js
+++ part4/ex4_3/tests/list_helper.test.js
@@ -7,18 +7,3 @@ test('dummy returns one', () => {
   expect(result).toBe(1)
 })
 
-describe('total likes', () => {
-  test('when list has only one blog, equals the likes of that', () => {
-    const listWithOneBlog = [
-      {
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-      },
-    ];
-
-    const result = listHelper.totalLikes(listWithOneBlog);
-    expect(result).toBe(5);
-  });
-});
--- part4/ex4_4/utils/list_helper.js
+++ part4/ex4_4/utils/list_helper.js
@@ -1,6 +1,11 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+  const likesArray = blogs.map((blog) => blog.likes);
+  return likesArray.reduce((sum, likes) => sum + likes, 0);
+};
   
 module.exports = {
   dummy
